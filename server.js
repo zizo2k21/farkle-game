f80237aacbef37d667b49a6c2c60ce6a16557cd8
habibHadjar <lhp6.hhadjar@gmail.com>
2023-03-15 11:27:15 +0100

    port 8080é

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 import healthcheck from './routes/healthcheck.js'
 dotenv.config()
 
-const PORT = process.env.PORT || 80
+const PORT = process.env.PORT || 8080
 const app = express()
 
 app.use(express.json())
